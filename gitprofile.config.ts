9d2a11c5a60b138d33e91ece7c9dc66cef995a00
Geoffrey Grossthal <104024651+geoffreygrossthal@users.noreply.github.com>
2024-06-13 07:55:46 -0700

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -29,7 +29,7 @@ const CONFIG = {
       },
     },
     external: {
-      header: 'My Projects',
+      header: '',
       // To hide the `External Projects` section, keep it empty.
       projects: [
         {
